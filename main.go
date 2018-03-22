ebd392bdde6d2915f68baa568330f4dc22f57035
Andres Orozco <ao15@my.fsu.edu>
2018-03-22 16:21:41 -0400

    add

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
 func main() {
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fs := http.FileServer(http.Dir("assets"))
 	http.Handle("/", fs)
 	log.Println("Starting Server...")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(addr, nil)
 }
