9a896893dec2ccbdd0fd1df63d7ee3382d2494db
Andres Orozco <ao15@my.fsu.edu>
2018-05-07 22:42:01 -0400

    use std lib for fileserver

--- main.go
+++ main.go
@@ -6,28 +6,25 @@ import (
 	"net/http"
 
 	_ "github.com/andresoro/home/statik"
-	"github.com/rakyll/statik/fs"
 )
 
 func main() {
 	var entry string
-	//var static string
+	var static string
 	var port string
 
 	// Handle command input
 	flag.StringVar(&entry, "entry", "./assets/index.html", "entry point")
-	// flag.StringVar(&static, "static", "./assets", "directory to serve static files")
-	flag.StringVar(&port, "port", "8080", "port to host server")
+	flag.StringVar(&static, "static", "./assets", "directory to serve static files")
+	flag.StringVar(&port, "port", ":8080", "port to host server")
 	flag.Parse()
 
 	// Handle Static Assets
-	statikFS, err := fs.New()
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.Handle("/assets", http.FileServer(http.Dir(static)))
 
+	// Handle Index
 	http.HandleFunc("/", IndexHandler(entry))
-	http.Handle("/assets", http.FileServer(statikFS))
+
 	log.Fatal(http.ListenAndServe(port, nil))
 }
 
